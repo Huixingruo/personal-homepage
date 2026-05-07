f1c6133d1884c9266de81d8105efb9f34fe22ec6
huixingruo <huixingruo@users.noreply.github.com>
2026-05-07 18:15:50 +0800

    feat: 组装所有组件，完成个人主页

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
-  return (
-    <div>
-      <h1>Personal Homepage</h1>
-    </div>
-  )
-}
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Awards from './components/Awards';
+import Contact from './components/Contact';
 
-export default App
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Awards />
+      <Contact />
+    </>
+  );
+}
